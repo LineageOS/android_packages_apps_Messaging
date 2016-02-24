ef4a818407c4903a9acc2b56e426fe9b52bce4d4
chengzhi.hou <chengzhi.hou@ck-telecom.com>
2016-02-24 20:01:17 +0800

    Messaging:Swipe right to delete conversation
    Change "swipe delete" to "swipe right delete", so SwipeArchive and SwipeDelete
    can exist at the same time.
    
    Change-Id: Ic03c850cecfb3546108a94e9af82eaa714b8d4e1

--- src/com/android/messaging/ui/conversationlist/ConversationListSwipeHelper.java
+++ src/com/android/messaging/ui/conversationlist/ConversationListSwipeHelper.java
@@ -269,7 +269,7 @@ public class ConversationListSwipeHelper implements OnItemTouchListener {
     private void onSwipeGestureEnd(final ConversationListItemView itemView,
             final int swipeDirection) {
         if (swipeDirection == SWIPE_DIRECTION_RIGHT || swipeDirection == SWIPE_DIRECTION_LEFT) {
-            itemView.onSwipeComplete();
+            itemView.onSwipeComplete(swipeDirection);
         }
 
         // Balances out onSwipeGestureStart.
