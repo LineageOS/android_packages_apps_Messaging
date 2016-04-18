3a2992e7f18563520d94c0abecc7fdabdc31aace
jaffersulaiman <jaffersulaiman.mh@sasken.com>
2016-04-18 16:50:51 +0530

    Showing Normalized number instead of denormalize number
    Normalized Number is showing for Regex BlackList
    Number
    
    Bug:CYNGNOS-2113
    Change-Id: Ie01e6938b9f3357cf7e8ffa59aa4c32c5e32d71f

--- src/com/android/messaging/util/BlacklistObserver.java
+++ src/com/android/messaging/util/BlacklistObserver.java
@@ -31,7 +31,6 @@ import com.android.messaging.datamodel.action.UpdateConversationArchiveStatusAct
 import com.android.messaging.datamodel.data.ParticipantData;
 import com.android.messaging.util.LogUtil;
 
-
 // ContentObserver class to monitor changes to the Framework Blacklist DB
 public class BlacklistObserver extends ContentObserver {
 
@@ -63,7 +62,8 @@ public class BlacklistObserver extends ContentObserver {
                     cursor = mResolver.query(uri, null, null, null, null);
                     int normalizedNumberIndex = cursor.getColumnIndex("normalized_number");
                     int blockedIndex = cursor.getColumnIndex("message");
-
+                    int nonNormalizedNumberIndex = cursor.getColumnIndex("number");
+                    int regexIndex = cursor.getColumnIndex("is_regex");
                     // if the column indices are not valid, don't perform the queries
                     if (normalizedNumberIndex < 0 || blockedIndex < 0) {
                         if (cursor != null) {
@@ -78,7 +78,8 @@ public class BlacklistObserver extends ContentObserver {
                         String number = cursor.getString(normalizedNumberIndex);
                         String blocked = cursor.getString(blockedIndex);
                         boolean isBlocked = blocked.compareTo("1") == 0;
-
+                        String formattedNumber = cursor.getInt(regexIndex) != 0
+                                ? cursor.getString(nonNormalizedNumberIndex) : null;
                         // don't update the framework db - the 'false' argument
                         int updateCount = BugleDatabaseOperations.updateDestination(db, number,
                                 isBlocked, false);
@@ -88,7 +89,7 @@ public class BlacklistObserver extends ContentObserver {
                             // create a new participant
                             // and insert him into the local participants database
                             ParticipantData participant =
-                                    ParticipantData.getFromRawPhoneBySystemLocale(number);
+                                    ParticipantData.getFromRawPhoneBySystemLocale(number, formattedNumber);
                             BugleDatabaseOperations.getOrCreateParticipantInTransaction(db,
                                     participant);
                             BugleDatabaseOperations.updateDestination(db, number,
