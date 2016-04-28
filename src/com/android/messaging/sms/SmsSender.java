492395420fde12ab8b0aa24a5dea339388602837
ashish <ashish.prabhakar@sasken.com>
2016-04-28 10:27:28 +0530

    Toast while sending SMS without Network service
    Toast message will display when try to send SMS and Network
    service will not be available.
    
    Change-Id: Ida25bbc1c800f2979eb1298f6fa4396309d1cfaf

--- src/com/android/messaging/sms/SmsSender.java
+++ src/com/android/messaging/sms/SmsSender.java
@@ -40,6 +40,8 @@ import com.cyanogenmod.messaging.util.PrefsUtils;
 
 import java.util.ArrayList;
 import java.util.Random;
+import com.android.messaging.util.BuglePrefs;
+import com.android.messaging.util.BuglePrefsKeys;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -140,6 +142,13 @@ public class SmsSender {
         }
     }
 
+    private static int getRetry() {
+        final BuglePrefs prefs = Factory.get().getApplicationPrefs();
+        final int retryAttempt =
+                prefs.getInt(BuglePrefsKeys.PROCESS_PENDING_MESSAGES_RETRY_COUNT, 0);
+        return retryAttempt;
+    }
+
     public static void setResult(final Uri requestId, final int resultCode,
             final int errorCode, final int partId, int subId) {
         if (resultCode != Activity.RESULT_OK) {
@@ -149,6 +158,10 @@ public class SmsSender {
             if (errorCode != SendStatusReceiver.NO_ERROR_CODE) {
                 final Context context = Factory.get().getApplicationContext();
                 UiUtils.showToastAtBottom(getSendErrorToastMessage(context, subId, errorCode));
+            }
+            final int retry = getRetry();
+            if (retry == 0 && (resultCode == SmsManager.RESULT_ERROR_NO_SERVICE)) {
+                UiUtils.showToastAtBottom(R.string.message_queued);
             }
         } else {
             if (LogUtil.isLoggable(TAG, LogUtil.VERBOSE)) {
