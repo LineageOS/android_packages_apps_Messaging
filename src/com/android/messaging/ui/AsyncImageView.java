5a7052fc29fd0e014e04421fd675256e01276268
Yvonne Wong <ywong@cyngn.com>
2016-05-11 14:02:12 -0700

    Add maps preview for addresses and integrate ridesharing request into Messaging
    - Uses the first map link found in a message to get a static map view
    - The static map view is shown as a preview and can be clicked to go into
    the default maps app
    - Clicking on request ride will lead the user to RideCoreUI with pickup
    location prefilled based on the address in the map link
    - Clicking on the directions button will lead the user to google maps's
    navigation
    - Display placeholder background when map is loading or offline with no cached
    image
    - Use RoundedCornerTransformation to transform the map into a bitmap with rounded
    corners
    - Show brand bitmap from ridesharing services if an active provider is chosen,
    otherwise display the generic ridesharing icon
    
    Change-Id: I8e1c1a25dba65ad0a7f46326ebe08d4adb9b41dc
    Issue-Id: RIDE-75, RIDE-76, RIDE-77

--- src/com/android/messaging/ui/AsyncImageView.java
+++ src/com/android/messaging/ui/AsyncImageView.java
@@ -113,8 +113,6 @@ public class AsyncImageView extends ImageView implements MediaResourceLoadListen
         attr.recycle();
     }
 
-
-
     /**
      * The main entrypoint for AsyncImageView to load image resource given an ImageRequestDescriptor
      * @param descriptor the request descriptor, or null if no image should be displayed
