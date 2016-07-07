cbaeca9095f885b7548b279cd57d7edd33d6f7ff
Danesh M <danesh@cyngn.com>
2016-07-07 12:28:18 -0700

    Messaging : Pass subId when calculating message length
    Change-Id: Ic060d64a291f8ecc7a5c0f010ec823c8f4176d08

--- src/com/android/messaging/datamodel/MessageTextStats.java
+++ src/com/android/messaging/datamodel/MessageTextStats.java
@@ -42,7 +42,7 @@ public class MessageTextStats {
     }
 
     public void updateMessageTextStats(final int selfSubId, final String messageText) {
-        final int[] params = SmsMessage.calculateLength(messageText, false);
+        final int[] params = SmsMessage.calculateLength(messageText, false, selfSubId);
         /* SmsMessage.calculateLength returns an int[4] with:
          *   int[0] being the number of SMS's required,
          *   int[1] the number of code points used,
