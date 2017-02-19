1b980eb78da0129ab036df4179d538ff3fa4062a
Scott Warner <Tortel1210@gmail.com>
2017-02-18 21:59:41 -0500

    messaging: Remove GMS dependency
    Change-Id: I55496b0c23e43726b92a73104943e23c43f715be

--- Android.mk
+++ Android.mk
@@ -68,7 +68,6 @@ LOCAL_AAPT_FLAGS += --extra-packages com.android.ex.photo
 LOCAL_AAPT_FLAGS += --extra-packages com.android.colorpicker
 LOCAL_AAPT_FLAGS += --extra-packages com.android.contacts.common
 LOCAL_AAPT_FLAGS += --extra-packages com.android.phone.common
-LOCAT_AAPT_FLAGS += --extra-packages com.google.android.gms
 
 ifdef TARGET_BUILD_APPS
     LOCAL_JNI_SHARED_LIBRARIES := libframesequence libgiftranscode
