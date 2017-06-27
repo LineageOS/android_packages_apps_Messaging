fdc0ba572f5e205dcbab81117aeba018104c63b4
Luca Stefani <luca020400@lineageos.org>
2017-06-27 18:09:13 +0200

    Messaging: Don't crash on unsupported shared content type
    Change-Id: Ibbd164ecf73ed5b7d4af0cdc0ca670085e96633c

--- src/com/android/messaging/ui/conversationlist/ShareIntentActivity.java
+++ src/com/android/messaging/ui/conversationlist/ShareIntentActivity.java
@@ -35,10 +35,13 @@ import com.android.messaging.util.ContentType;
 import com.android.messaging.util.LogUtil;
 import com.android.messaging.util.MediaMetadataRetrieverWrapper;
 import com.android.messaging.util.FileUtil;
+import com.android.messaging.util.UiUtils;
 import com.android.messaging.util.UriUtil;
 
 import java.io.IOException;
 import java.util.ArrayList;
+
+import com.android.messaging.R;
 
 public class ShareIntentActivity extends BaseBugleActivity implements
         ShareIntentFragment.HostInterface {
@@ -107,7 +110,10 @@ public class ShareIntentActivity extends BaseBugleActivity implements
                 }
             } else {
                 // Unsupported content type.
-                Assert.fail("Unsupported shared content type for " + contentUri + ": " + contentType
+                finish();
+                UiUtils.showToastAtBottom(R.string.attachment_load_failed_dialog_message);
+                LogUtil.e(LogUtil.BUGLE_TAG,
+                        "Unsupported shared content type for " + contentUri + ": " + contentType
                         + " (" + intent.getType() + ")");
             }
         } else if (Intent.ACTION_SEND_MULTIPLE.equals(action)) {
@@ -133,7 +139,9 @@ public class ShareIntentActivity extends BaseBugleActivity implements
                 }
             } else {
                 // Unsupported content type.
-                Assert.fail("Unsupported shared content type: " + contentType);
+                finish();
+                UiUtils.showToastAtBottom(R.string.attachment_load_failed_dialog_message);
+                LogUtil.e(LogUtil.BUGLE_TAG, "Unsupported shared content type: " + contentType);
             }
         } else {
             // Unsupported action.
