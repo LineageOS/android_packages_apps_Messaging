850fca565c6b2cc7bc33dc50d6a6fd88b0f56b68
Kevin F. Haggerty <haggertk@lineageos.org>
2018-08-07 18:58:00 -0600

    Revert "Messaging: Request external storage permission before handling shared files"
    * Change Ibff486c94ac703a9a4c5a80e33b934a460804a8f from AOSP removes
      ability to share file URIs via intent, noting that the capability
      is "disallowed by Android N and later", therefore this request is
      no longer relevant.
    
    This reverts commit 19274aab7e07a0707e6b6671fd65554844268a56.
    
    Change-Id: I99fbd9f8ff81dda96a7422912e9d55a35533e0a9

--- src/com/android/messaging/ui/conversationlist/ShareIntentActivity.java
+++ src/com/android/messaging/ui/conversationlist/ShareIntentActivity.java
@@ -16,7 +16,6 @@
 
 package com.android.messaging.ui.conversationlist;
 
-import android.Manifest;
 import android.app.Fragment;
 import android.content.ContentResolver;
 import android.content.Intent;
@@ -36,9 +35,7 @@ import com.android.messaging.util.ContentType;
 import com.android.messaging.util.LogUtil;
 import com.android.messaging.util.MediaMetadataRetrieverWrapper;
 import com.android.messaging.util.FileUtil;
-import com.android.messaging.util.OsUtil;
 import com.android.messaging.util.UiUtils;
-import com.android.messaging.util.UriUtil;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -81,10 +78,6 @@ public class ShareIntentActivity extends BaseBugleActivity implements
         final String action = intent.getAction();
         if (Intent.ACTION_SEND.equals(action)) {
             final Uri contentUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
-            if (UriUtil.isFileUri(contentUri) && !OsUtil.hasStoragePermission()) {
-                requestPermissions(
-                        new String[] { Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
-            }
             final String contentType = extractContentType(contentUri, intent.getType());
             if (LogUtil.isLoggable(LogUtil.BUGLE_TAG, LogUtil.DEBUG)) {
                 LogUtil.d(LogUtil.BUGLE_TAG, String.format(
@@ -125,10 +118,6 @@ public class ShareIntentActivity extends BaseBugleActivity implements
                 if (imageUris != null && imageUris.size() > 0) {
                     mDraftMessage = MessageData.createSharedMessage(null);
                     for (final Uri imageUri : imageUris) {
-                        if (UriUtil.isFileUri(imageUri) && !OsUtil.hasStoragePermission()) {
-                            requestPermissions(
-                                    new String[] { Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
-                        }
                         final String actualContentType = extractContentType(imageUri, contentType);
                         addSharedImagePartToDraft(actualContentType, imageUri);
                     }
