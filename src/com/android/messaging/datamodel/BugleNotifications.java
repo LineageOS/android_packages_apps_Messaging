76f810e00d70385309c89cdb8aed6a725cc7fa0f
Paul Keith <javelinanddart@gmail.com>
2018-10-30 15:46:18 +0100

    Messaging: Add "Mark as read" quick action for message notifications
    Change-Id: I7194dca022e5062926fa35709de282721ca64320

--- src/com/android/messaging/datamodel/BugleNotifications.java
+++ src/com/android/messaging/datamodel/BugleNotifications.java
@@ -807,6 +807,13 @@ public class BugleNotifications {
             wearableExtender.setBackground(defaultBackground);
         }
 
+        final PendingIntent readPendingIntent = notificationState.getReadIntent();
+        final NotificationCompat.Action.Builder readActionBuilder =
+                new NotificationCompat.Action.Builder(R.drawable.ic_wear_read,
+                        context.getString(R.string.notification_mark_as_read), readPendingIntent);
+        notifBuilder.addAction(readActionBuilder.build());
+        wearableExtender.addAction(readActionBuilder.build());
+
         if (notificationState instanceof MultiMessageNotificationState) {
             if (attachmentBitmap != null) {
                 // When we've got a picture attachment, we do some switcheroo trickery. When
@@ -842,7 +849,7 @@ public class BugleNotifications {
             maybeAddWearableConversationLog(wearableExtender,
                     (MultiMessageNotificationState) notificationState);
             addDownloadMmsAction(notifBuilder, wearableExtender, notificationState);
-            addWearableVoiceReplyAction(wearableExtender, notificationState);
+            addReplyAction(notifBuilder, wearableExtender, notificationState);
         }
 
         // Apply the wearable options and build & post the notification
@@ -884,7 +891,7 @@ public class BugleNotifications {
         }
     }
 
-    private static void addWearableVoiceReplyAction(
+    private static void addReplyAction(final NotificationCompat.Builder notifBuilder,
             final WearableExtender wearableExtender, final NotificationState notificationState) {
         if (!(notificationState instanceof MultiMessageNotificationState)) {
             return;
@@ -921,6 +928,7 @@ public class BugleNotifications {
                 setChoices(choices)
                 .build();
         actionBuilder.addRemoteInput(remoteInput);
+        notifBuilder.addAction(actionBuilder.build());
         wearableExtender.addAction(actionBuilder.build());
     }
 
