606029be695cae07ce4b05a689acbe33bb8052b0
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-03-16 16:27:01 +0100

    Revert "Fixed storage permission issue for attachments"
    This reverts commit 2f0f7aee6432f475e84bb4873e7777542757645e.
    
    Change-Id: I4f697eae9d525f7c284dcfabb30ff0def07fb375

--- src/com/android/messaging/ui/mediapicker/AudioListChooser.java
+++ src/com/android/messaging/ui/mediapicker/AudioListChooser.java
@@ -174,9 +174,6 @@ class AudioListChooser extends MediaChooser implements
             // Work around a bug in MediaStore where cursors querying the Files provider don't get
             // updated for changes to Images.Media or Video.Media.
             startMediaPickerDataLoader();
-            updateForPermissionState(true);
-        } else {
-            updateForPermissionState(false);
         }
     }
 
@@ -186,7 +183,7 @@ class AudioListChooser extends MediaChooser implements
         if (selected && !OsUtil.hasStoragePermission()) {
             mMediaPicker.requestPermissions(
                     new String[] { Manifest.permission.READ_EXTERNAL_STORAGE },
-                    MediaPicker.AUDIO_LIBRARY_PERMISSION_REQUEST_CODE);
+                    MediaPicker.GALLERY_PERMISSION_REQUEST_CODE);
         }
     }
 
@@ -198,7 +195,7 @@ class AudioListChooser extends MediaChooser implements
     @Override
     protected void onRequestPermissionsResult(
             final int requestCode, final String permissions[], final int[] grantResults) {
-        if (requestCode == MediaPicker.AUDIO_LIBRARY_PERMISSION_REQUEST_CODE) {
+        if (requestCode == MediaPicker.GALLERY_PERMISSION_REQUEST_CODE) {
             final boolean permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
             if (permissionGranted) {
                 startMediaPickerDataLoader();
