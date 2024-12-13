099321e6236bdfa2a645bec06546568655889992
Michael W <baddaemon87@gmail.com>
2024-12-13 15:11:32 +0100

    Messaging: Replace ExifInterface with its androidx counterpart
    Change-Id: I35ba14886b9f837d576f1cd385869614454c921f

--- src/com/android/messaging/datamodel/media/SimSelectorAvatarRequest.java
+++ src/com/android/messaging/datamodel/media/SimSelectorAvatarRequest.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2015 The Android Open Source Project
+ * Copyright (C) 2024 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,8 +27,9 @@ import android.graphics.PorterDuffColorFilter;
 import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.graphics.drawable.BitmapDrawable;
-import android.media.ExifInterface;
 import android.text.TextUtils;
+
+import androidx.exifinterface.media.ExifInterface;
 
 import com.android.messaging.R;
 import com.android.messaging.util.Assert;
