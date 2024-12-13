ad871f0d8419a6cd94ad46dc92b935d7dbcd1d2e
Michael W <baddaemon87@gmail.com>
2024-12-13 20:39:09 +0100

    Messaging: Nullability
    Change-Id: I8bcde2a868a9c8989971d056be24ff4986e4f8f3

--- src/com/android/messaging/util/NotificationPlayer.java
+++ src/com/android/messaging/util/NotificationPlayer.java
@@ -25,6 +25,8 @@ import android.os.Looper;
 import android.os.PowerManager;
 import android.os.SystemClock;
 
+import androidx.annotation.NonNull;
+
 import com.android.messaging.Factory;
 
 import java.util.LinkedList;
@@ -53,6 +55,7 @@ public class NotificationPlayer implements OnCompletionListener {
         long requestTime;
         boolean releaseFocus;
 
+        @NonNull
         @Override
         public String toString() {
             return "{ code=" + code + " looping=" + looping + " stream=" + stream
