35ed6a04135f638ef0917e7cf69b7f106000d4db
Michael W <baddaemon87@gmail.com>
2024-12-16 15:49:00 +0100

    Messaging: Convert further parts to androidx
    CursorLoader, LoaderManager, Preferences, Fragments, ...
    
    Change-Id: I8787b2db365249839547c6d5c21ab7cd5ff42082

--- src/com/android/messaging/util/ChangeDefaultSmsAppHelper.java
+++ src/com/android/messaging/util/ChangeDefaultSmsAppHelper.java
@@ -17,10 +17,11 @@
 package com.android.messaging.util;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.view.View;
+
+import androidx.fragment.app.Fragment;
 
 import com.android.messaging.R;
 import com.android.messaging.ui.SnackBar;
