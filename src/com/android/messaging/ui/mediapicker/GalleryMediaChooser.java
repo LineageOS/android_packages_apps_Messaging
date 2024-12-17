be839d54c83d07e3449c8a4ff91532a090b0be06
Michael W <baddaemon87@gmail.com>
2024-12-17 15:51:34 +0100

    Messaging: Fix C-style array declarations
    Change-Id: Ie7381dc5280ab4d907000ef551df9ea80553ddb4

--- src/com/android/messaging/ui/mediapicker/GalleryMediaChooser.java
+++ src/com/android/messaging/ui/mediapicker/GalleryMediaChooser.java
@@ -224,7 +224,7 @@ class GalleryMediaChooser extends MediaChooser implements
 
     @Override
     protected void onRequestPermissionsResult(
-            final int requestCode, final String permissions[], final int[] grantResults) {
+            final int requestCode, final String[] permissions, final int[] grantResults) {
         if (requestCode == MediaPicker.GALLERY_PERMISSION_REQUEST_CODE) {
             final boolean permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
             if (permissionGranted) {
