8c2d83181c8ea3ea6b660b83ef7fdee54d2b1992
Michael W <baddaemon87@gmail.com>
2024-12-23 17:43:42 +0100

    Messaging: ContactMediaChooser: Remove permission requesting
    We never reach this state when we remove the permission, so it makes
    no sense to have code for it
    
    Change-Id: I97f2f5d438148234ea1887462ab65735721da2f8

--- src/com/android/messaging/ui/mediapicker/ContactMediaChooser.java
+++ src/com/android/messaging/ui/mediapicker/ContactMediaChooser.java
@@ -16,11 +16,9 @@
 
 package com.android.messaging.ui.mediapicker;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract.Contacts;
@@ -33,7 +31,6 @@ import androidx.activity.result.contract.ActivityResultContracts;
 
 import com.android.messaging.R;
 import com.android.messaging.datamodel.data.PendingAttachmentData;
-import com.android.messaging.util.ContactUtil;
 import com.android.messaging.util.ContentType;
 import com.android.messaging.util.LogUtil;
 import com.android.messaging.util.SafeAsyncTask;
@@ -45,8 +42,6 @@ import com.android.messaging.util.UiUtils;
  * set of permissions for this app. Thus no case to request READ_CONTACTS permission on it actually.
  */
 class ContactMediaChooser extends MediaChooser {
-    private View mEnabledView;
-    private View mMissingPermissionView;
     private final ActivityResultLauncher<Intent> mPickerLauncher;
 
     ContactMediaChooser(final MediaPicker mediaPicker) {
@@ -113,9 +108,8 @@ class ContactMediaChooser extends MediaChooser {
                         R.layout.mediapicker_contact_chooser,
                         container /* root */,
                         false /* attachToRoot */);
-        mEnabledView = view.findViewById(R.id.mediapicker_enabled);
-        mMissingPermissionView = view.findViewById(R.id.missing_permission_view);
-        mEnabledView.setOnClickListener(v -> {
+        final View enabledView = view.findViewById(R.id.mediapicker_enabled);
+        enabledView.setOnClickListener(v -> {
             // Launch an external picker to pick a contact as attachment.
             final Intent intent = new Intent(Intent.ACTION_PICK, Contacts.CONTENT_URI);
 
@@ -127,25 +121,5 @@ class ContactMediaChooser extends MediaChooser {
             }
         });
         return view;
-    }
-
-    @Override
-    protected void setSelected(final boolean selected) {
-        super.setSelected(selected);
-        if (selected && !ContactUtil.hasReadContactsPermission()) {
-            mMediaPicker.requestPermissions(
-                    new String[] {Manifest.permission.READ_CONTACTS},
-                    MediaPicker.READ_CONTACT_PERMISSION_REQUEST_CODE);
-        }
-    }
-
-    @Override
-    protected void onRequestPermissionsResult(
-            final int requestCode, final String[] permissions, final int[] grantResults) {
-        if (requestCode == MediaPicker.READ_CONTACT_PERMISSION_REQUEST_CODE) {
-            final boolean permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-            mEnabledView.setVisibility(permissionGranted ? View.VISIBLE : View.GONE);
-            mMissingPermissionView.setVisibility(permissionGranted ? View.GONE : View.VISIBLE);
-        }
     }
 }
--- src/com/android/messaging/ui/mediapicker/MediaChooser.java
+++ src/com/android/messaging/ui/mediapicker/MediaChooser.java
@@ -204,6 +204,4 @@ abstract class MediaChooser extends BasePagerViewHolder
     /** Optional activity life-cycle methods to be overridden by subclasses */
     public void onPause() { }
     public void onResume() { }
-    protected void onRequestPermissionsResult(
-            final int requestCode, final String[] permissions, final int[] grantResults) { }
 }
