3dd9381ea15274da5d288eade231dba2f2671813
Michael W <baddaemon87@gmail.com>
2024-12-25 23:39:55 +0100

    Messaging: Style even more dialogs properly
    Somehow some dialogs use the theme but some need the style set manually
    
    Change-Id: I386a0e43c10d328088066961b75a162bf2c351c1

--- src/com/android/messaging/ui/conversationsettings/CopyContactDetailDialog.java
+++ src/com/android/messaging/ui/conversationsettings/CopyContactDetailDialog.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2015 The Android Open Source Project
+ * Copyright (C) 2024 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,7 +39,7 @@ public class CopyContactDetailDialog implements DialogInterface.OnClickListener 
     }
 
     public void show() {
-        new AlertDialog.Builder(mContext)
+        new AlertDialog.Builder(mContext, R.style.AlertDialogTheme)
                 .setView(createBodyView())
                 .setTitle(R.string.copy_to_clipboard_dialog_title)
                 .setPositiveButton(R.string.copy_to_clipboard, this)
