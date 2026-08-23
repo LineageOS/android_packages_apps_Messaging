6d131ed8e7249b0ece41169e17d8d0162beae7e0
SaeedDev94 <saeedp47@gmail.com>
2026-08-23 13:47:55 -0500

    Messaging: Fix white background revealed by conversation list swipe in dark theme
    Change-Id: Ifb9bb7dfed7ea7b5a8f1308de99d53f7103400d7

--- src/com/android/messaging/ui/conversationlist/ConversationListItemView.java
+++ src/com/android/messaging/ui/conversationlist/ConversationListItemView.java
@@ -539,7 +539,7 @@ public class ConversationListItemView extends FrameLayout implements OnClickList
                 mCrossSwipeArchiveLeftImageView.setVisibility(GONE);
                 mCrossSwipeArchiveRightImageView.setVisibility(VISIBLE);
             }
-            mSwipeableContainer.setBackgroundResource(R.drawable.swipe_shadow_drag);
+            mSwipeableContainer.setBackgroundResource(R.color.window_background);
         }
     }
 
